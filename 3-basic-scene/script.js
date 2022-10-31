c6d769bf43f41fe6d6ac65feab3ffcae255a609d
Tommy Palmer <hi@tommyp.org>
2022-10-31 09:33:48 +0000

    3 basic scene

---
+++ 3-basic-scene/script.js
@@ -0,0 +1,30 @@
+// element
+const canvas = document.querySelector('canvas.webgl');
+
+// scene
+const scene = new THREE.Scene();
+
+// red cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// sizes
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+// camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+scene.add(camera);
+camera.position.z = 3;
+
+// renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+});
+
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
